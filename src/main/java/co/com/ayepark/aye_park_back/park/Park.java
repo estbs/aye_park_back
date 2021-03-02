5a73f4ec3807099a655db239fcf30e9b97dbb392
Esteban BS <estbs93@gmail.com>
2021-03-01 21:32:46 -0500

    Add fields to Park model

--- src/main/java/co/com/ayepark/aye_park_back/park/Park.java
+++ src/main/java/co/com/ayepark/aye_park_back/park/Park.java
@@ -1,8 +1,63 @@
 package co.com.ayepark.aye_park_back.park;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import co.com.ayepark.aye_park_back.user.User;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
 
 @Entity
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Park {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id")
+    User user;
+
+    String name;
+
+    String address;
+
+    String nit;
+
+    String country;
+
+    String city;
+
+    @Column(name = "auto_cells_number")
+    Integer autoCellsNumber;
+
+    @Column(name = "motorcycle_cells_number")
+    Integer motorcycleCellsNumber;
+
+    @CreatedDate
+    LocalDateTime createdAt;
+
+    @LastModifiedDate
+    LocalDateTime updatedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="created_by")
+    User createdBy;
+
 }
