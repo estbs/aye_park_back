47f2da3f98510b42a6fc0d79ae059351b80c800b
Esteban BS <estbs93@gmail.com>
2021-03-01 21:33:06 -0500

    Add fields to User model

--- src/main/java/co/com/ayepark/aye_park_back/user/User.java
+++ src/main/java/co/com/ayepark/aye_park_back/user/User.java
@@ -1,8 +1,50 @@
 package co.com.ayepark.aye_park_back.user;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 @Entity
 public class User {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String name;
+
+    String lastname;
+
+    String username;
+
+    @Column(name = "document_type")
+    String documentType;
+
+    @Column(name = "document_number")
+    String documentNumber;
+
+    String email;
+
+    String address;
+
+    @Column(name = "cellphone_number")
+    String cellphoneNumber;
+
+    @CreatedDate
+    LocalDateTime createdAt;
+
+    @LastModifiedDate
+    LocalDateTime updatedAt;
+
+    Integer createdBy;
 }
